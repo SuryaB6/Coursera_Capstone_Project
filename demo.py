ebb1eb9531785e7ccdaeff5f438fb3446d5b51c4
SuryaB6 <39698374+SuryaB6@users.noreply.github.com>
2020-06-21 11:58:04 +0530

    Add files via upload
    This is demo for coursera capstone project

---
+++ demo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+
+# ## This notebook is used for Coursera Capstone project
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
